d2bdb06d69e61119585cd9a8eecd7a322686c394
xiandel <110621182+xiandel@users.noreply.github.com>
2022-08-09 08:04:05 +0100

    update

---
+++ TEST-PLAN.md
@@ -0,0 +1,12 @@
+There really is not enough information to write a test plan there are some important things to put in place.
+
+Prior to development during tool selection, I would expect documentation reviews.
+
+From the testing perspective this should be done with the testing methodology in mind.  Early in the process (particularly if we are planning to use automation) it would be worth confirming that the chosen system is compatible with the tool that is used.
+
+Development specifications should include ID or classes for objects this would enable a comprehensive set of test scripts to be created alongside development.
+
+ Alongside the automated tests I would do some manual tests when released to a live website I would issue a statement to customers advising that they confirm any roles are still showing this would fulfil the role of a beta test. If you are going to get really technical, I know it’s possible to only release the changes to a limited number of customers (amazon, does it) but I’m unsure if this would be within scope for this website (I have no idea how hard it is to do). A benefit to this is that it’s possible to monitor live performance.
+
+Speaking of which some sort of performance test would be required. 
+
--- cypress/integration/jobs_search.spec.js
+++ cypress/integration/jobs_search.spec.js
@@ -1,22 +1,58 @@
 /// <reference types="cypress" />
 
 context('Jobs Search', () => {
-    beforeEach(() => {
-      cy.visit('/')
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+
+  it('verify search results', () => {
+
+    cy.wait(3000);
+    cy.waitFor('#onetrust-accept-btn-handler')
+    cy.get('#onetrust-accept-btn-handler').click()
+    cy.get('.header_content__LmDsk > .button_base__KzM17').click()
+    cy.get('h1').should('have.text', "Find a Care Assistant job")
+    cy.wait(3000);
+    cy.get('.location-search-input').focus().type('Trafford', { force: true })
+    cy.get('.locationSearchInput_dropdown__KG52s').within(() => {
+      cy.contains('Trafford Park')
+      cy.contains('Bridge Trafford')
+
     })
-  
-    // Existing and non-existing location
-    // wait for results
-    it('verify search results', () => {
+
+
+  })
+  //Negative 1 checks that incorrect answers are not present
+  it('negative scenario 1', () => {
+
+    cy.waitFor('#onetrust-accept-btn-handler')
+    cy.get('#onetrust-accept-btn-handler').click()
+    cy.get('.header_content__LmDsk > .button_base__KzM17').click()
+    cy.get('h1').should('have.text', "Find a Care Assistant job")
+    cy.wait(3000);
+    cy.get('.location-search-input').focus().type('Trafford', { force: true })
+    cy.get('.locationSearchInput_dropdown__KG52s').within(() => {
+      cy.contains('Manchester').should('not.exist')
+
+    })
+  })
+
+    //Negative 2 checks that the correct answer are not present for a different search location
+    it('negative senario 2', () => {
+
+
+      cy.waitFor('#onetrust-accept-btn-handler')
       cy.get('#onetrust-accept-btn-handler').click()
       cy.get('.header_content__LmDsk > .button_base__KzM17').click()
       cy.get('h1').should('have.text', "Find a Care Assistant job")
       cy.wait(3000);
-      cy.get('.location-search-input').focus().type('Trafford', { force: true })
+      cy.get('.location-search-input').focus().type('Manchester', { force: true })
       cy.get('.locationSearchInput_dropdown__KG52s').within(() => {
-          cy.contains('Manchester')
-      })
-    })
+        cy.contains('Trafford Park').should('not.exist')
+        cy.contains('Bridge Trafford').should('not.exist')
 
-    // it('negative scenario', () => {})
-})
+      })
+
+    })
+  })
